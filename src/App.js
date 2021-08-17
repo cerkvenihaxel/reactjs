352c7d083e3da1b2373d3df544b12ef6fdba45c1
cerkvenihaxel <cerkvenihaxel@gmail.com>
2021-08-17 18:53:20 -0300

    Centrado de botón y objetos

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Button, Modal, ModalHeader, ModalBody, ModalFooter, FormGroup, Input, Label} from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.css';
-import {Table, Button, Container, Modal, ModalBody, ModalHeader, FormGroup, ModalFooters} from 'reactstrap';
-import React from 'react';
 
 class App extends React.Component{
+  state={
+    abierto: false,
+  }
 
+  abrirModal=()=>{
+    this.setState({abierto: !this.state.abierto});
+  }
 
   render(){
 
+    const modalStyles={
+      position: "absolute",
+      top: '50%',
+      left: '50%',
+      transform: 'translate(-50%, -50%)'
+    }
     return(
+      <>
+      <div className="principal">
+        <div className="secundario">
+      <Button color="success" onClick={this.abrirModal}>Mostrar Modal</Button>
 
+      </div></div>
 
-      <Button color="primary"> Insertar Nuevo Personaje</Button>
+      <Modal isOpen={this.state.abierto} style={modalStyles}>
+        <ModalHeader>
+          Iniciar Sesión
+        </ModalHeader>
+        <ModalBody>
+          <FormGroup>
+            <Label for="usuario">Usuario</Label>
+            <Input type="text" id="usuario"/> 
+          </FormGroup>
+          <FormGroup>
+            <Label for="password">Contraseña</Label>
+            <Input type="password" id="password"/> 
+          </FormGroup>
+        </ModalBody>
+
+        <ModalFooter>
+            <Button color="primary">Iniciar Sesión</Button>
+            <Button color="secondary" onClick={this.abrirModal}>Cerrar</Button>
+        </ModalFooter>
+      </Modal>
+      </>
     )
-
-
   }
-
-
-
 }
+
+export default App;
