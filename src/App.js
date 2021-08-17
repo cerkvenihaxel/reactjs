faa1326ce3242a65f48b9c430e5b1a0834f08abe
cerkvenihaxel <cerkvenihaxel@gmail.com>
2021-08-17 13:02:40 -0300

    Lo nuevo

--- src/App.js
+++ src/App.js
@@ -2,26 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import {Table, Button, Container, Modal, ModalBody, ModalHeader, FormGroup, ModalFooters} from 'reactstrap';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          QUE COSA<code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Aprendé react ahora
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+
+
+  render(){
+
+    return(
+
+
+      <Button color="primary"> Insertar Nuevo Personaje</Button>
+    )
+
+
+  }
+
+
+
 }
-
-export default App;
